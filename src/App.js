0516b641d2502c290ac09a6f65d7331d900e6d40
oliFYP <k2026944@kingston.ac.uk>
2024-02-06 17:37:08 +0000

    added website link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css"; // Import the CSS file
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi!</h1>
+      <p>
+        My name is <strong>Francisco</strong>.
+        <br />
+        I'm a Portuguese Graphic Designer,
+        <br />
+        based in London, UK.
+      </p>
     </div>
   );
 }
