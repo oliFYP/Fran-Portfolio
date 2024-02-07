4a0e5fb43f1ea56ee29271835538e67d16bacd7c
oliFYP <k2026944@kingston.ac.uk>
2024-02-07 15:35:06 +0000

    Added profile, set up margins, added logo and language changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 // App.js
 import React from "react";
-import logo from "./logo.svg";
+import Fran from "./Fran.png";
+import logo from "./logo.png";
 import "./App.css"; // Import the CSS file
-
+import "./Mobile-App.css";
 function App() {
   return (
     <div className="App">
+      <div className="Top-bar">
+        <img className="logo" src={logo}></img>
+        <div className="languages">
+          <p>PT</p>
+          <p>|</p>
+          <p>EN</p>
+        </div>
+      </div>
       <h1>Hi!</h1>
-      <p>
+      <p className="intro">
         My name is <strong>Francisco</strong>.
         <br />
         I'm a Portuguese Graphic Designer,
         <br />
         based in London, UK.
       </p>
+      <div className="buttons">
+        <button className="About-Us">About Us</button>
+        <img src={Fran} alt="Fran" className="Profile"></img>
+        <button className="Portfolio">Portfolio</button>
+      </div>
     </div>
   );
 }
