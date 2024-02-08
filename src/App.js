da2ddc7dc0791b38bea8d5541c08b9f85955a927
oliFYP <k2026944@kingston.ac.uk>
2024-02-08 19:48:21 +0000

    added a switch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
-// App.js
-import React from "react";
+import React, { useState } from "react";
+import Switch from "react-switch";
 import Fran from "./Fran.png";
 import logo from "./logo.png";
-import "./App.css"; // Import the CSS file
+import "./App.css";
 import "./Mobile-App.css";
+
 function App() {
+  const [currentLanguage, setCurrentLanguage] = useState("EN");
+  const [checked, setChecked] = useState(false);
+
+  const toggleLanguage = () => {
+    setCurrentLanguage((prevLanguage) => (prevLanguage === "EN" ? "PT" : "EN"));
+  };
+
+  const handleChange = (checked) => {
+    setChecked(checked);
+  };
+
   return (
     <div className="App">
-      <div className="Top-bar">
-        <img className="logo" src={logo}></img>
-        <div className="languages">
-          <p>PT</p>
-          <p>|</p>
-          <p>EN</p>
+      <div className="section-1">
+        <div className="Top-bar">
+          <img className="logo" src={logo} alt="Logo" />
+          <div className="languages" onClick={toggleLanguage}>
+            <p className={`pt ${currentLanguage === "PT" ? "active" : ""}`}>
+              PT
+            </p>
+            <Switch
+              className="switch-language"
+              onChange={handleChange}
+              checked={checked}
+              uncheckedIcon={null}
+              checkedIcon={null}
+              onColor="#000080"
+              offColor="#006847"
+            />
+
+            <p className={`en ${currentLanguage === "EN" ? "active" : ""}`}>
+              EN
+            </p>
+          </div>
+        </div>
+        <h1>Hi!</h1>
+        <p className="intro">
+          My name is <strong>Francisco</strong>.
+          <br />
+          I'm a Portuguese Graphic Designer,
+          <br />
+          based in London, UK.
+        </p>
+        <div className="buttons">
+          <button className="About-Us">About Us</button>
+          <img src={Fran} alt="Fran" className="Profile" />
+          <button className="Portfolio">Portfolio</button>
         </div>
       </div>
-      <h1>Hi!</h1>
-      <p className="intro">
-        My name is <strong>Francisco</strong>.
-        <br />
-        I'm a Portuguese Graphic Designer,
-        <br />
-        based in London, UK.
-      </p>
-      <div className="buttons">
-        <button className="About-Us">About Us</button>
-        <img src={Fran} alt="Fran" className="Profile"></img>
-        <button className="Portfolio">Portfolio</button>
-      </div>
+      <div className="section-2"></div>
     </div>
   );
 }
